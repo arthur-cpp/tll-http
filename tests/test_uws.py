3ef08fd62b360c52c66948600fec93dfc6a1e575
Pavel Shramov <psha@nguni.psha.org.ru>
2022-01-07 19:42:28 +0300

    uws: Use curl:// in uws tests

--- tests/test_uws.py
+++ tests/test_uws.py
@@ -1,109 +1,89 @@
 #!/usr/bin/env python3
 # vim: sts=4 sw=4 et
 
-import http.client
+import decorator
 import os
 import pytest
-import threading
-import urllib.request
 
 from tll import asynctll
 from tll.channel import Context
+from tll.test_util import ports
 
-class Test:
-    def setup(self):
-        self.ctx = Context()
-        self.ctx.load(os.path.join(os.environ.get("BUILD_DIR", "build"), "tll-uws"), 'channel_module')
-        self.loop = asynctll.Loop(context=self.ctx)
+@pytest.fixture
+def context():
+    ctx = Context()
+    try:
+        ctx.load(os.path.join(os.environ.get("BUILD_DIR", "build"), "tll-uws"), 'channel_module')
+        ctx.load(os.path.join(os.environ.get("BUILD_DIR", "build"), "tll-curl"), 'channel_module')
+    except:
+        pytest.skip("uws:// or curl:// channels not available")
+    return ctx
 
-        self.server = self.loop.Channel('ws://*:8080', name='server')
-        self.thread_event = self.loop.Channel("direct://", name='thread/tll', dump='text')
-        self.main_event = self.ctx.Channel("direct://", name='thread/main', master=self.thread_event)
-        self.channels = []
+@pytest.fixture
+def server(asyncloop, port):
+    c = asyncloop.Channel(f'ws://*:{port}', name='server')
+    yield c
+    c.close()
 
-        self.thread = None
-        self.thread_stop = threading.Event()
-        self.thread_error = None
+@pytest.fixture
+def asyncloop(context):
+    loop = asynctll.Loop(context)
+    yield loop
+    loop.destroy()
+    loop = None
 
-        self.thread_event.open()
-        self.main_event.open()
+@pytest.fixture
+def port():
+    return ports.TCP6
 
-    def teardown(self):
-        if self.thread:
-            self.thread_stop.set()
-            self.thread.join()
-        self.thread = None
-        self.thread_stop = None
-        self.thread_error = None
+@decorator.decorator
+def asyncloop_run(f, asyncloop, *a, **kw):
+    asyncloop.run(f(asyncloop, *a, **kw))
 
-        if self.loop:
-            self.loop.stop = 1
-            self.loop = None
+@pytest.fixture
+def client(asyncloop, port):
+    c = asyncloop.Channel(f'curl+http://127.0.0.1:{port}', transfer='control', name='client', dump='frame')
+    yield c
+    c.close()
+    del c
 
-        for c in self.channels + [self.thread_event, self.main_event]:
-            c.close()
+async def check_response(c, addr, connect={}, data=None):
+    m = await c.recv()
+    assert (m.type, m.addr) == (m.Type.Control, addr)
+    m = c.unpack(m)
+    assert m.SCHEME.name == 'connect'
+    assert {k:v for k,v in m.as_dict().items() if k in connect} == connect
 
-        self.thread_event = None
-        self.main_event = None
-        self.channels = []
+    m = await c.recv()
+    assert (m.type, m.addr) == (m.Type.Data, addr)
+    if data is not None:
+        assert m.data == data
 
-        if self.server:
-            self.server.close()
-            self.server = None
-        self.ctx = None
+    m = await c.recv()
+    assert (m.type, m.addr) == (m.Type.Control, addr)
+    assert c.unpack(m).SCHEME.name == 'disconnect'
 
-    def http(self, *a, **kw):
-        conn = http.client.HTTPConnection('::1', 8080, timeout=1)
-        conn.request(*a, **kw)
-        return conn.getresponse()
+@asyncloop_run
+async def test_http(asyncloop, server, client):
+    sub = asyncloop.Channel("ws+http://path", master=server, name='server/http', dump='yes');
 
-    def test_http(self):
-        sub = self.loop.Channel("ws+http://path", master=self.server, name='server/http', dump='yes');
+    server.open()
+    client.open()
 
-        self.channels = [sub]
+    client.post({'path':'/path'}, type=client.Type.Control, name='connect', addr=1)
+    await check_response(client, 1, {'code':404})
 
-        self.server.open()
+    sub.open()
 
-        def process(self, sub):
-            async def main():
-                m = await self.thread_event.recv()
-                assert m.data.tobytes() == b'open'
+    client.post({'path':'/abc'}, type=client.Type.Control, name='connect', addr=2)
+    await check_response(client, 2, {'code':404})
 
-                sub.open()
+    client.post({'path':'/path'}, type=client.Type.Control, name='connect', addr=3)
 
-                m = await sub.recv()
-                assert m.type == m.Type.Control
+    m = await sub.recv()
 
-                sub.post(b'hello', addr=m.addr)
+    assert m.type == m.Type.Control
+    
+    sub.post(b'hello', addr=m.addr)
 
-                m = await self.thread_event.recv()
-                assert m.data.tobytes() == b'done'
-            try:
-                self.loop.run(main())
-            except Exception as e:
-                self.thread_error = e
-                raise
-
-        self.thread = threading.Thread(target=process, args=(self, sub))
-        self.thread.start()
-
-        r = self.http('GET', '/')
-        assert r.status == 404
-        assert self.thread_error == None, self.thread_error
-
-        self.main_event.post(b'open')
-
-        r = self.http('GET', '/')
-        assert r.status == 404
-        assert self.thread_error == None, self.thread_error
-
-        r = self.http('GET', '/path')
-        assert r.status == 200
-        assert r.read() == b'hello'
-        assert self.thread_error == None, self.thread_error
-
-        self.main_event.post(b'done')
-        self.loop.stop = 1
-
-        self.thread.join()
-        assert self.thread_error == None, self.thread_error
+    await check_response(client, 3, {'code':200}, b'hello')
