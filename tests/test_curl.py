27d93a95b3819a3b23bfc3c726a47486e2a969f1
Pavel Shramov <psha@nguni.psha.org.ru>
2024-09-13 12:31:00 +0300

    tests: Use relaxed enum comparison
    Scheme enums can be compared to enum.Enum with same name

--- tests/test_curl.py
+++ tests/test_curl.py
@@ -7,6 +7,7 @@ from tll.error import TLLError
 from tll.test_util import ports
 
 import decorator
+import enum
 import http.server
 import os
 import pytest
@@ -86,8 +87,8 @@ def httpd(port):
 def asyncloop_run(f, asyncloop, *a, **kw):
     asyncloop.run(f(asyncloop, *a, **kw))
 
-def UNDEFINED(c):
-    return c.scheme_control.enums['Method'].klass.UNDEFINED
+class Method(enum.Enum):
+    UNDEFINED = -1
 
 @asyncloop_run
 async def test_autoclose(asyncloop, port, httpd):
@@ -100,7 +101,7 @@ async def test_autoclose(asyncloop, port, httpd):
 
     m = await c.recv()
     assert m.type == m.Type.Control
-    assert c.unpack(m).as_dict() == {'code': 200, 'method': UNDEFINED(c), 'headers': HEADERS, 'path': f'http://[::1]:{port}/some/path', 'size': -1}
+    assert c.unpack(m).as_dict() == {'code': 200, 'method': Method.UNDEFINED, 'headers': HEADERS, 'path': f'http://[::1]:{port}/some/path', 'size': -1}
 
     m = await c.recv()
     assert m.data.tobytes() == b'GET /some/path'
@@ -125,7 +126,7 @@ async def test_autoclose_many(asyncloop, port, httpd):
 
     m = await c0.recv()
     assert m.type == m.Type.Control
-    assert c0.unpack(m).as_dict() == {'code': 200, 'method': UNDEFINED(c0), 'headers': HEADERS, 'path': f'http://[::1]:{port}/c0', 'size': -1}
+    assert c0.unpack(m).as_dict() == {'code': 200, 'method': Method.UNDEFINED, 'headers': HEADERS, 'path': f'http://[::1]:{port}/c0', 'size': -1}
 
     m = await c0.recv(0.11)
     assert m.data.tobytes() == b'GET /c0'
@@ -136,7 +137,7 @@ async def test_autoclose_many(asyncloop, port, httpd):
     assert m.type == m.Type.Control
     assert c1.unpack(m).as_dict() == {
         'code': 500,
-        'method': UNDEFINED(c1),
+        'method': Method.UNDEFINED,
         'size': 10,
         'headers': [{'header': 'content-length', 'value': '10'}] + HEADERS,
         'path': f'http://[::1]:{port}/c1',
@@ -172,7 +173,7 @@ async def test_data(asyncloop, port, httpd):
         assert m.addr == 0
         assert c.unpack(m).as_dict() == {
             'code': 500,
-            'method': UNDEFINED(c),
+            'method': Method.UNDEFINED,
             'size': 12 + len(data),
             'headers': [{'header': 'content-length', 'value': str(12 + len(data))}] + HEADERS + [{'header': 'x-test-header', 'value': 'value'}],
             'path': f'http://[::1]:{port}/post',
@@ -207,7 +208,7 @@ async def test_data(asyncloop, port, httpd):
         assert m.addr == addr
         assert c.unpack(m).as_dict() == {
             'code': 500,
-            'method': UNDEFINED(c),
+            'method': Method.UNDEFINED,
             'size': 12 + len(data),
             'headers': [{'header': 'content-length', 'value': str(12 + len(data))}] + HEADERS + [{'header': 'x-test-header', 'value': 'value'}],
             'path': f'http://[::1]:{port}/post',
@@ -291,7 +292,7 @@ async def test_control(asyncloop, port, httpd):
         assert m.addr == i
         assert c.unpack(m).as_dict() == {
             'code': 500,
-            'method': UNDEFINED(c),
+            'method': Method.UNDEFINED,
             'size': 20 + len(d),
             'headers': [{'header': 'content-length', 'value': str(20 + len(d))}] + HEADERS + [{'header': 'x-test-header', 'value': str(i)}],
             'path': f'http://[::1]:{port}/post/extra/{i}',
