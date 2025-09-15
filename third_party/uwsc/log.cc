e66055df4200601b1b0c6e786f749de69b8bb78f
Pavel Shramov <psha@lebu.psha.org.ru>
2025-09-15 17:21:04 +0300

    uwsc: Fix invalid logger pointer conversion

--- third_party/uwsc/log.cc
+++ third_party/uwsc/log.cc
@@ -2,21 +2,24 @@
 #include <atomic>
 #include <mutex>
 
-static std::unique_ptr<tll::Logger> _uwsc_logger;
+static tll_logger_t * _uwsc_logger;
 static unsigned _uwsc_logger_ref = 0;
 static std::mutex _uwsc_logger_lock;
 
-tll_logger_t * uwsc_logger() { return (tll_logger_t *) _uwsc_logger.get(); }
+tll_logger_t * uwsc_logger() { return _uwsc_logger; }
+
 void uwsc_logger_ref()
 {
 	auto lock = std::unique_lock<std::mutex>(_uwsc_logger_lock);
 	if (_uwsc_logger_ref++ == 0)
-		_uwsc_logger.reset(new tll::Logger("uwsc"));
+		_uwsc_logger = tll_logger_new("uwsc", -1);
 }
 
 void uwsc_logger_unref()
 {
 	auto lock = std::unique_lock<std::mutex>(_uwsc_logger_lock);
-	if (--_uwsc_logger_ref == 0)
-		_uwsc_logger.reset();
+	if (--_uwsc_logger_ref == 0) {
+		tll_logger_free(_uwsc_logger);
+		_uwsc_logger = nullptr;
+	}
 }
